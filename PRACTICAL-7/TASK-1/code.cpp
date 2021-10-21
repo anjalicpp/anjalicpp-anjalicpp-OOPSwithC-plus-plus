9df8167becef57f8dd9e3f7b6ba112f9357bf209
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2021-10-21 21:21:28 +0530

    Create code.cpp

---
+++ PRACTICAL-7/TASK-1/code.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{int ch;
+cin>>ch;
+switch(ch){
+case 1:cout<<"YEAR=2021";cout<<endl;break;
+case 2:cout<<"MONTH=October";<<endl;break;
+case 3:cout<<"DATE=18";<<endl;break;
+default:cout<<"INVALID CHOICE";cout<<endl;}
+}
