09782c7b770f125b7f7ebe5327448552a50d076e
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2021-10-21 21:31:35 +0530

    Create code.cpp

---
+++ PRACTICAL-7/TASK4/4-A/code.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{int m[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+
+for(int i=0;i<3;i++)
+{ for(int j=0;j<3;j++)
+m[i][j]=i+1;}
+
+for(int i=0;i<3;i++)
+{
+for(int j=0;j<3;j++)
+cput<<m[i][j]<<" ";}
+cout<<endl;}
