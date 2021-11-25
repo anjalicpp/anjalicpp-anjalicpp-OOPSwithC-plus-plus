3ef0f5dcb706cc1763ba34d677e228cbe82c9166
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2021-11-25 19:49:22 +0530

    Update code.cpp

--- PRACTICAL-2/code.cpp
+++ PRACTICAL-2/code.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{int num,flag,i;
+cout<<"Entre the number:"
+cin>>num;
+for(i=2;i<num;i++)
+if(num%i==0)
+{flag=1;break;}
+if(flag) cout<<"not prime";
+cout<<"prime";
+return 0;
+}
 
