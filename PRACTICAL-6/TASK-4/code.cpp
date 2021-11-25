cedbc8ddd7e9a9e4b937a759fa5dc8a04b1dd45d
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2021-11-25 19:55:56 +0530

    Create code.cpp

---
+++ PRACTICAL-6/TASK-4/code.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+ 
+void relationaloperator(string s1,string s2)
+{
+if(s1!=s2)
+cout<<"not equal"<<endl;
+else cout<<equal<<endl;
+}
+int main()
+{string st1,st2;
+getline(cin,st1);
+getline(cin,st2);}
+relationoperation(s1,s2);
+return 0;}
