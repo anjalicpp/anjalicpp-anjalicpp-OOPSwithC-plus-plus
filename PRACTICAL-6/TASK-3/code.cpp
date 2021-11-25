2ecd6ed526bbc835ddc2f4d33614bb801cacda16
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2021-11-25 19:54:48 +0530

    Create code.cpp

---
+++ PRACTICAL-6/TASK-3/code.cpp
@@ -0,0 +1,27 @@
+//reverse function
+//reversefunction
+#include<bits/stdc++.h>
+
+using namespace std;
+ 
+int main()
+{string str;
+getline(cin,str);
+reverse(str.begin(),str.end());
+cout<<str<<\n;
+return 0;}
+
+//manualfunction
+#include<bits/stdc++.h>
+
+using namespace std;
+void reverse(string str)
+{for(int i=str.length()-1;i>=0;i--)
+cout<<str[i];}
+ 
+int main(){
+string str;
+getline(cin,str);reverse(s);
+cout<<\n;
+return 0;}
+
