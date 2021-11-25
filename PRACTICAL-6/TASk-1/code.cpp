5f10b930e27d8539518d55c0b2e2581c3b6e05b5
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2021-11-25 19:52:02 +0530

    Update code.cpp

--- PRACTICAL-6/TASk-1/code.cpp
+++ PRACTICAL-6/TASk-1/code.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+ 
+int main()
+{
+int a,b;float c,d;
+string s1,s2;
+cout<<"Enter the integer value";
+cin>>a>>b;
+cout<<"Enter float values";
+cin>>c>>d;
+cout<<"Enter the string values";cin>>string1;cin>>string2;
+cout<<"Add int&float value"<<a+c;
+cout<<"Add string & int:"<<a+string1;
+return 0;}
